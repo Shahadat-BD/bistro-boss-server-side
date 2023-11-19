45a22161ac41ffade0211db2b2b99b6f3b68ece7
Shahadat-BD <shahadatstudent34@gmail.com>
2023-11-20 01:54:06 +0600

    token and admin verified done

--- index.js
+++ index.js
@@ -63,6 +63,36 @@ async function run() {
         const result = await foodCartCollection.deleteOne(query)
         res.send(result)
     })
+
+     //  middleware 
+      
+     const verifyToken = (req,res,next) => { 
+        // console.log('inside verify token',req.headers.authorization);
+         if (!req.headers.authorization) { 
+              return res.status(401).send({message:"unauthorized access"})
+         }
+         const token = req.headers.authorization.split(' ')[1]
+         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded) => {
+            if (err) {
+                return res.status(401).send({message:"unauthorized access"})
+            }
+            req.decoded = decoded 
+            next()
+         })
+    }
+
+    // verified admin 
+    // user verify admin after verifyToken
+    const verifyAdmin = async(req,res,next)=>{
+       const email = req.decoded.email
+       const query = {email: email}
+       const user = await userCollection.findOne(query)
+       const isAdmin = user?.role === 'admin'
+       if (!isAdmin) {
+           return res.status(403).send({message:'forbidden access'})
+       }
+       next()
+    }
           
     // user name and email added in database by POST API
     app.post('/user',async(req,res)=>{
@@ -77,15 +107,29 @@ async function run() {
         const result = await userCollection.insertOne(user)
         res.send(result)
     })   
-
+   
     // collect all users by GET API
-    app.get('/user', async (req, res) => {
+    app.get('/user',verifyToken, verifyAdmin, async (req, res) => {
         const result = await userCollection.find().toArray()
         res.send(result)
     })
 
+    app.get('/user/admin/:email',verifyToken,async(req,res)=>{
+       const email  = req.params.email
+       if (email !== req.decoded.email) {
+          return res.status(403).send({message:"forbidden access"})
+       }
+       const query = {email : email};
+       const user = await userCollection.findOne(query)
+       let admin = false
+       if (user) {
+           admin = user?.role === 'admin'
+       }
+       res.send({admin})
+    }) 
+
     // delete user by DELETE API
-    app.delete('/user/:id',async(req,res)=>{
+    app.delete('/user/:id', verifyToken , verifyAdmin, async(req,res)=>{
         const userId = req.params.id
         const query = {_id : new ObjectId(userId)}
         const result = await userCollection.deleteOne(query)
@@ -93,7 +137,7 @@ async function run() {
     })
 
     // admin created by PATCH API
-    app.patch('/user/admin/:id',async(req,res)=>{
+    app.patch('/user/admin/:id',verifyToken,verifyAdmin, async(req,res)=>{
         const userId = req.params.id
         const filter = {_id : new ObjectId(userId)}
         const updateDoc = {
@@ -103,6 +147,13 @@ async function run() {
         }
         const result = await userCollection.updateOne(filter,updateDoc)
         res.send(result)
+    })
+
+    // jwt related api
+    app.post('/jwt',async(req,res)=>{
+        const user = req.body 
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+        res.send({token})
     })
  
         // Send a ping to confirm a successful connection
